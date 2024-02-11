01a474243f6bdfed0a1b4dc882b16966b936e093
Fedox <67076970+Fedox-die-Ente@users.noreply.github.com>
2024-02-11 13:48:15 +0100

    [LOGIN_FORM] Moved file

--- lib/ui/widgets/login/LoginForm.dart
+++ lib/ui/widgets/login/LoginForm.dart
@@ -1,15 +1,13 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../../blocs/authentication/authentication_bloc.dart';
-import '../../blocs/authentication/authentication_event.dart';
-import '../../blocs/login/login_bloc.dart';
-import '../../blocs/login/login_event.dart';
-import '../../blocs/login/login_state.dart';
-import '../../repositories/userRepository.dart';
+import '../../../blocs/authentication/authentication_bloc.dart';
+import '../../../blocs/authentication/authentication_event.dart';
+import '../../../blocs/login/login_bloc.dart';
+import '../../../blocs/login/login_event.dart';
+import '../../../blocs/login/login_state.dart';
+import '../../../repositories/userRepository.dart';
 
 class LoginForm extends StatefulWidget {
   final UserRepository _userRepository;
@@ -76,43 +74,41 @@ class _LoginFormState extends State<LoginForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: BlocListener<LoginBloc, LoginState>(
-
         listener: (context, state) {
           if (state.isFailure) {
             ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-
-                  content: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text("Login failed. Check your credentials."),
-                      Icon(color: Colors.red, Icons.error_outline)
-                    ],
-                  ),
+              const SnackBar(
+                content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Login failed. Check your credentials."),
+                    Icon(color: Colors.red, Icons.error_outline)
+                  ],
                 ),
-              );
+              ),
+            );
           }
 
           if (state.isSubmitting) {
             ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text("Logging In..."),
-                      CircularProgressIndicator(),
-                    ],
-                  ),
+              const SnackBar(
+                content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Logging In..."),
+                    CircularProgressIndicator(),
+                  ],
                 ),
-              );
+              ),
+            );
           }
 
           if (state.isSuccess) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
             Navigator.of(context).pop();
             BlocProvider.of<AuthenticationBloc>(context).add(LoggedIn());
           }
         },
-
         child: BlocBuilder<LoginBloc, LoginState>(
           builder: (context, state) {
             // Use the state to build your UI
@@ -131,19 +127,23 @@ class _LoginFormState extends State<LoginForm> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  const SizedBox(height: 80,),
+                  const SizedBox(
+                    height: 80,
+                  ),
                   Padding(
                     padding: const EdgeInsets.all(20),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text("Login", style: GoogleFonts.redHatDisplay(
-                            color: Colors.white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold)),
+                        Text("Login",
+                            style: GoogleFonts.redHatDisplay(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold)),
                         const SizedBox(height: 10),
-                        Text("Welcome back on BerryBuddy.", style: GoogleFonts
-                            .redHatDisplay(color: Colors.white, fontSize: 18)),
+                        Text("Welcome back on BerryBuddy.",
+                            style: GoogleFonts.redHatDisplay(
+                                color: Colors.white, fontSize: 18)),
                       ],
                     ),
                   ),
@@ -152,53 +152,54 @@ class _LoginFormState extends State<LoginForm> {
                     child: Container(
                       decoration: const BoxDecoration(
                           color: Colors.black54,
-                          borderRadius: BorderRadius.only(topLeft: Radius
-                              .circular(60), topRight: Radius.circular(60))
-                      ),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(60),
+                              topRight: Radius.circular(60))),
                       child: Padding(
                         padding: const EdgeInsets.all(30),
                         child: Column(
                           children: <Widget>[
-                            const SizedBox(height: 60,),
+                            const SizedBox(
+                              height: 60,
+                            ),
                             Container(
                               decoration: BoxDecoration(
                                   color: Colors.white,
                                   borderRadius: BorderRadius.circular(10),
-                                  boxShadow: const [BoxShadow(
-                                      color: Color.fromRGBO(60, 21, 117, .3),
-                                      blurRadius: 20,
-                                      offset: Offset(0, 10)
-                                  )
-                                  ]
-                              ),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                        color: Color.fromRGBO(60, 21, 117, .3),
+                                        blurRadius: 20,
+                                        offset: Offset(0, 10))
+                                  ]),
                               child: Column(
                                 children: <Widget>[
                                   Container(
                                     padding: const EdgeInsets.all(10),
                                     decoration: BoxDecoration(
-                                        border: Border(bottom: BorderSide(
-                                            color: Colors.deepPurpleAccent
-                                                .shade200))
-                                    ),
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.deepPurpleAccent
+                                                    .shade200))),
                                     child: TextFormField(
                                       controller: _emailController,
                                       validator: (_) {
-                                        return !state.isEmailValid ? "Invalid email" : null;
+                                        return !state.isEmailValid
+                                            ? "Invalid email"
+                                            : null;
                                       },
                                       decoration: const InputDecoration(
                                           hintText: "Email or phone number",
-                                          hintStyle: TextStyle(
-                                              color: Colors.grey),
-                                          border: InputBorder.none
-                                      ),
+                                          hintStyle:
+                                              TextStyle(color: Colors.grey),
+                                          border: InputBorder.none),
                                     ),
                                   ),
-
                                   Container(
                                     padding: const EdgeInsets.all(10),
                                     decoration: const BoxDecoration(
-                                      //border: Border(bottom: BorderSide(color: Colors.grey.shade200))
-                                    ),
+                                        //border: Border(bottom: BorderSide(color: Colors.grey.shade200))
+                                        ),
                                     child: TextFormField(
                                       controller: _passwordController,
                                       obscureText: true,
@@ -209,16 +210,17 @@ class _LoginFormState extends State<LoginForm> {
                                       },
                                       decoration: const InputDecoration(
                                           hintText: "Password",
-                                          hintStyle: TextStyle(
-                                              color: Colors.grey),
-                                          border: InputBorder.none
-                                      ),
+                                          hintStyle:
+                                              TextStyle(color: Colors.grey),
+                                          border: InputBorder.none),
                                     ),
                                   ),
                                 ],
                               ),
                             ),
-                            const SizedBox(height: 40,),
+                            const SizedBox(
+                              height: 40,
+                            ),
                             // FadeInUp(duration: const Duration(milliseconds: 1500), child: Text("Forgot Password?", style: GoogleFonts.redHatDisplay(color: Colors.grey),)),
                             InkWell(
                               onTap: () {
@@ -231,7 +233,9 @@ class _LoginFormState extends State<LoginForm> {
                               ),
                             ),
 
-                            const SizedBox(height: 40,),
+                            const SizedBox(
+                              height: 40,
+                            ),
                             MaterialButton(
                               onPressed: () {
                                 isLoginButtonEnabled(state)
@@ -239,20 +243,25 @@ class _LoginFormState extends State<LoginForm> {
                                     : null;
                               },
                               height: 50,
-                              color: isLoginButtonEnabled(state) ? Colors.purple[900] : Colors.grey,
+                              color: isLoginButtonEnabled(state)
+                                  ? Colors.purple[900]
+                                  : Colors.grey,
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(50),
                               ),
                               child: Center(
-                                child: Text("Login", style: GoogleFonts.redHatDisplay(color: Colors.white, fontWeight: FontWeight.bold)),
+                                child: Text("Login",
+                                    style: GoogleFonts.redHatDisplay(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold)),
                               ),
                             ),
-                            const SizedBox(height: 40,),
+                            const SizedBox(
+                              height: 40,
+                            ),
 
                             InkWell(
-                              onTap: () {
-
-                              },
+                              onTap: () {},
                               child: Text(
                                   "Don't have an account? Create one here.",
                                   style: GoogleFonts.redHatDisplay(

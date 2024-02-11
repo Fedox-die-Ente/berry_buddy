00381ba54e55690b579d70b4b06280733d36e9c8
Austria7 <austria71@outlook.de>
2024-02-11 04:28:50 +0100

    [API/CORS] Fix allowedOrigins Error

--- de.fedustria.berrybuddy.api/src/main/java/de/fedustria/berrybuddy/api/rest/WebConfig.java
+++ de.fedustria.berrybuddy.api/src/main/java/de/fedustria/berrybuddy/api/rest/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }

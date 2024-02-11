bcfd144eb393f4bd0f3839453a64d42d28e9578a
Fedox <67076970+Fedox-die-Ente@users.noreply.github.com>
2024-02-11 13:48:05 +0100

    [LOGIN] Fixed imports

--- lib/ui/pages/login.dart
+++ lib/ui/pages/login.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../blocs/login/login_bloc.dart';
 import '../../repositories/userRepository.dart';
-import '../../widgets/login/LoginForm.dart';
+import '../widgets/login/LoginForm.dart';
 
 class Login extends StatelessWidget {
   final UserRepository _userRepository;

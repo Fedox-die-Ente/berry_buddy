aca995359e64b503ccf806685b99f1fed29edad8
Fedox <67076970+Fedox-die-Ente@users.noreply.github.com>
2024-02-11 13:48:35 +0100

    [MAIN] Fixes and redesign

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:berry_buddy/blocs/authentication/authentication_bloc.dart';
 import 'package:berry_buddy/blocs/authentication/authentication_state.dart';
 import 'package:berry_buddy/repositories/userRepository.dart';
-import 'package:berry_buddy/ui/pages/firstprofile.dart';
 import 'package:berry_buddy/ui/pages/home.dart';
-import 'package:berry_buddy/ui/pages/login.dart';
 import 'package:berry_buddy/ui/pages/splash.dart';
-import 'package:berry_buddy/ui/pages/test.dart';
-import 'package:berry_buddy/widgets/tabs.dart';
+import 'package:berry_buddy/ui/widgets/tabs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'blocs/authentication/authentication_event.dart';
-import 'blocs/blocDelegate.dart';
 import 'constants.dart';
-import 'utils/notify.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,8 +21,9 @@ void main() {
     MultiBlocProvider(
       providers: [
         BlocProvider<AuthenticationBloc>(
-          create: (context) => AuthenticationBloc(userRepository: _userRepository)
-            ..add(AppStarted()),
+          create: (context) =>
+              AuthenticationBloc(userRepository: _userRepository)
+                ..add(AppStarted()),
         ),
       ],
       child: MyApp(userRepository: _userRepository),
@@ -44,38 +40,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'Berry Buddy',
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
-        scaffoldBackgroundColor: kBackgroundColor,
-        primarySwatch: Colors.deepPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      //home: Home(userRepository: _userRepository),
-      home: BlocBuilder<AuthenticationBloc, AuthenticationState> (
-        builder: (context, state) {
-          if(state is Uninitialized) {
+        debugShowCheckedModeBanner: false,
+        title: 'Berry Buddy',
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+          scaffoldBackgroundColor: kBackgroundColor,
+          primarySwatch: Colors.deepPurple,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        //home: Home(userRepository: _userRepository),
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+            builder: (context, state) {
+          // return Banned();
+          if (state is Uninitialized) {
             return Splash();
           }
           if (state is Authenticated) {
             return Tabs(
-              userId: _userRepository,
-            );
-          }
-          if (state is AuthenticatedButNotSet) {
-            return ProfileSetup(
-              userRepository: _userRepository,
               userId: state.userId,
             );
           }
-          if(state is Unauthenticated) {
+          if (state is AuthenticatedButNotSet) {
+            // return ProfileSetup(
+            //   userRepository: _userRepository,
+            //   userId: state.userId,
+            // );
+            return Tabs(
+              userId: state.userId,
+            );
+          }
+          if (state is Unauthenticated) {
             return Home(userRepository: _userRepository);
           } else {
             return Container();
           }
-        }
-      )
-    );
+        }));
   }
 }

41f75285fb166bd347c08093031aa86953b02ba7
Fedox <67076970+Fedox-die-Ente@users.noreply.github.com>
2024-02-11 15:05:45 +0100

    [PROFILE] Creating blocprovider now

--- lib/ui/pages/profile.dart
+++ lib/ui/pages/profile.dart
@@ -1,5 +1,9 @@
+import 'package:berry_buddy/repositories/userRepository.dart';
 import 'package:berry_buddy/ui/widgets/myProfileForm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../blocs/profile/profile_bloc.dart';
 
 class Profile extends StatefulWidget {
   final String userId;
@@ -13,6 +17,15 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return MyProfileForm(userId: widget.userId);
+    return Scaffold(
+      body: BlocProvider<ProfileBloc>(
+        create: (context) => ProfileBloc(
+          userRepository: UserRepository(),
+        ),
+        child: MyProfileForm(
+          userId: widget.userId,
+        ),
+      ),
+    );
   }
 }
